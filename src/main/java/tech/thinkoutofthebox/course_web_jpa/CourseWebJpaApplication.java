6f6dd30662931e4faca61befd903881934108719
Renato Leme <renatol1857@gmail.com>
2020-05-25 16:39:58 -0300

    Project created

---
+++ src/main/java/tech/thinkoutofthebox/course_web_jpa/CourseWebJpaApplication.java
@@ -0,0 +1,13 @@
+package tech.thinkoutofthebox.course_web_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWebJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/thinkoutofthebox/course_web_jpa/CourseWebJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.thinkoutofthebox.course_web_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
